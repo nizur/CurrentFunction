c62c53d9632af97717a55453cc6ad2f54b408439
Chris Ruzin <chris@solspace.com>
2012-01-12 23:30:02 -0600

    Initial commit

---
+++ CurrentFunction.py
@@ -0,0 +1,34 @@
+import sublime, sublime_plugin
+
+
+class FunctionInStatusListener(sublime_plugin.EventListener):
+    def on_deactived(self, view):
+        view.erase_status('function name')
+
+    def on_close(self, view):
+        view.erase_status('function name')
+
+    def on_activated(self, view):
+        cf = self.get_current_function(view)
+        if cf is None:
+            view.erase_status('function name')
+        else:
+            view.set_status('function name', 'Function: ' + cf)
+
+    def on_selection_modified(self, view):
+        cf = self.get_current_function(view)
+        if cf is None:
+            view.erase_status('function name')
+        else:
+            view.set_status('function name', 'Function: ' + cf)
+
+    def get_current_function(self, view):
+        sel = view.sel()[0]
+        functionRegs = view.find_by_selector('entity.name.function')
+        cf = None
+        for r in reversed(functionRegs):
+            if r.a < sel.a:
+                cf = view.substr(r)
+                break
+
+        return cf
